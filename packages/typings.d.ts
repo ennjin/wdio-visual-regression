1603627a7698639e9f0d1be1d9553af26619e150
Vitalie <ennjin5@gmail.com>
2020-01-27 22:38:57 +0300

    feat: create project

---
+++ packages/typings.d.ts
@@ -0,0 +1,26 @@
+declare module 'resemblejs/compareImages' {
+  type ImageData = string | Buffer;
+  
+  function compare(target: ImageData, compareWith: ImageData): Promise<CompareResult>;
+
+  interface CompareResult {
+    isSameDimensions: boolean;
+    dimensionDifference: {
+      width: number;
+      height: number;
+    };
+    rawMisMatchPercentage: number;
+    misMatchPercentage: number;
+    diffBounds: {
+      top: number;
+      left: number;
+      bottom: number;
+      right: number;
+    };
+    analysisTime: number;
+    getImageDataUrl(): string;
+    getBuffer(): Buffer;
+  }
+  
+  export = compare;
+}
