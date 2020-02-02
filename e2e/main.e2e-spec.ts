0f2d457b82a153b34fdc83028deb4670538046f1
Vitaly <ennjin@yandex.ru>
2020-02-02 22:18:56 +0300

    fix: crop image

--- e2e/main.e2e-spec.ts
+++ e2e/main.e2e-spec.ts
@@ -3,6 +3,6 @@
 describe('Visual regression', () => {
   it('Should check element', async () => {
     await browser.url(browser.config.baseUrl);
-    // const body = await browser.$('body');
+    // const body = await browser.$('body > div');
   });
 });
--- packages/service/image.ts
+++ packages/service/image.ts
@@ -1,3 +1,31 @@
-export class Image {
-// TODO
+import { createCanvas, loadImage } from 'canvas';
+
+const BASE64_PREFIX = 'data:image/png;base64,';
+
+
+export class DOMImage {
+  private element: WebdriverIOAsync.Element;
+
+  constructor(element: WebdriverIOAsync.Element) {
+    this.element = element;
+  }
+
+  private async getElementRect() {
+    const { x, y } = await this.element.getLocation();
+    const { width, height } = await this.element.getSize();
+    return { x, y, width, height }
+  }
+
+  private async takeElementScreenshot(): Promise<Buffer> {
+    // TODO: full pgae screenshot intead viewport
+    const base64 = await browser.takeScreenshot();
+    const image = await loadImage(BASE64_PREFIX + base64);
+
+    const { x, y, width, height } = await this.getElementRect();
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext('2d');
+    context.drawImage(image, x, y, width, height, 0, 0, width, height);
+    
+    return canvas.toBuffer();
+  }
 }
