a8b2d4a5d749b73abbc313948b2152dfcdc8e4c5
Vitalie <ennjin5@gmail.com>
2020-03-23 19:58:06 +0300

    feat: add typing for @wdio/sync

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,15 @@
-declare namespace WebdriverIOAsync {
+declare module 'webdriverio' {
   interface BrowserObject {
     matchElement(name: string, element: WebdriverIOAsync.Element): Promise<number>;
     matchViewport(name: string): Promise<number>;
   }
 }
 
+declare module '@wdio/sync' {
+  interface BrowserObject {
+    matchElement(name: string, element: WebdriverIO.Element): number;
+    matchViewport(name: string): number;
+  }
+}
+
+export {};
